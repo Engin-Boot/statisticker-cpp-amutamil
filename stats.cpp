f901793d3cd9731ce6ecf8ba29179541356d8c58
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-03 22:30:43 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,10 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& num ) {
     //Implement statistics here
+    Statistics::stats obj;
+    obj.avrg = obj.average(num);
+    obj.mini = obj.min(num);
+    obj.maxi = obj.max(num);
+    return obj;
 }
