87bca7f9efc31bf23dc8b14ad72577575967af16
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-03 22:31:33 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,53 @@
 #include <vector>
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    class stats{
+    public:
+        float avrg, maxi, mini;
+        float average(const vector<float>& n);
+        float max(const vector<float>& n);
+        float min(const vector<float>& n);
+    };
+    float stats::average(const vector<float>& n) {
+        float sum = 0;
+        for (unsigned int i = 0;i < n.size();i++)
+        {
+            sum += n[i];
+        }
+        return sum/n.size();
+    }
+    float stats::max(const vector<float>& n) {
+        float maximum = INT_MIN;
+        if (n.size() == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            for (unsigned int i = 0;i < n.size();i++)
+            {
+                if (n[i] > maximum)
+                    maximum = n[i];
+            }
+            return maximum;
+        }
+    }
+    float stats::min(const vector<float>& n) {
+        float minimum = INT_MAX;
+        if (n.size() == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            for (unsigned int i = 0;i < n.size();i++)
+            {
+                if (n[i] < minimum)
+                    minimum = n[i];
+            }
+            return minimum;
+        }
+    }
+    stats ComputeStatistics(const std::vector<float>& num);
+    
 }
