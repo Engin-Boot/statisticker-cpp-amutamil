409a3b03579f0fb5042a592dd3d32ce491edcc03
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-05 23:25:31 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,6 +4,13 @@ using namespace Statistics;
 stats Statistics::ComputeStatistics(const std::vector<float>& num ) {
     //Implement statistics here
     Statistics::stats obj;
+    if(num.size()==0)
+    {
+        obj.avrg = 0;
+        obj.mini = 0;
+        obj.maxi = 0;
+        return obj;
+    }
     obj.avrg = obj.average(num);
     obj.mini = obj.min(num);
     obj.maxi = obj.max(num);
