bf4d5a6bdb5c1ddec5575a2d7388d547bf94d13a
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-05 06:53:04 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& num ) {
+stats Statistics::ComputeStatistics(const std::vector<float>& num ) {
     //Implement statistics here
     Statistics::stats obj;
     obj.avrg = obj.average(num);
