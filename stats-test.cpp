e1565e8c0a1fe73f30d048c1f285e92f2010e16d
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-06 13:34:49 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -28,7 +28,14 @@ TEST_CASE("average, maximum and minimum is NaN for empty array") {
 TEST_CASE("average, minimum and maximum when NaN is input") {
     auto computedStats = Statistics::ComputeStatistics({nan(""),1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
-    //REQUIRE(std::abs(computedStats.avrg - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.avrg - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.maxi - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.mini - 1.5) < epsilon);
+}
+
+TEST_CASE("average, minimum and maximum when only one input and it is NaN") {
+    auto computedStats = Statistics::ComputeStatistics({nan("")});
+    REQUIRE(std::isnan(computedStats.avrg ) == true);
+    REQUIRE(std::isnan(computedStats.maxi ) == true);
+    REQUIRE(std::isnan(computedStats.mini ) == true);
 }
